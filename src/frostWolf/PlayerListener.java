a2db5589b087c5591c931073a6da8c4c4ce8e41e
FrostMiser <FrostMiser@users.noreply.github.com>
2017-01-16 14:43:51 -0600

    Add ability to become a FrostWolf by being bitten by a wolf.

--- src/frostWolf/PlayerListener.java
+++ src/frostWolf/PlayerListener.java
@@ -2,12 +2,16 @@ package frostWolf;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
+
 import org.bukkit.ChatColor;
 
 
@@ -17,7 +21,30 @@ public class PlayerListener implements Listener {
 	@EventHandler
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
 		
-		//If damager is a FrostWolf
+		
+		//Players being damaged by wolves in winter biomes have a chance to become a FrostWolf
+
+		//If damager is a wolf and entity being damaged is a player, and player is not already a FrostWolf
+		if (event.getDamager() instanceof Wolf && 
+			event.getEntity() instanceof Player && 
+			!(FrostWolf.wolfList.contains(event.getEntity().getUniqueId()))) {
+			
+			//Chance of player becoming a FrostWolf
+			Random randomGen = new Random();
+			int frostWolfChance = randomGen.nextInt(10);
+			
+			if (frostWolfChance == 5) {
+				event.getEntity().sendMessage(ChatColor.AQUA + "[FrostWolf] You feel the power of a FrostWolf flowing through you.");
+				//todo add inital effects
+				FrostWolf.wolfList.add(event.getEntity().getUniqueId());
+			}
+
+		}
+		
+		
+		
+		
+		//If damager is a FrostWolf, increase the amount of damage
 		if (!(event.getDamager() instanceof Player)) { return;} //Exit if damager is not a player
 		Player damager = (Player) event.getDamager();
 		if (!FrostWolf.wolfList.contains(damager.getUniqueId())) { return; } //Exit if damager is not a FrostWolf
